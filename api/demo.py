6cbd48567eb87c130b048b1cb14671f56158b5c9
Pranathi Poojary <pranathi.poojary20@gmail.com>
2025-02-15 11:38:00 +0000

    [IMP] Conn 2

--- api/demo.py
+++ api/demo.py
@@ -1,6 +1,12 @@
 from httpx import Request
 
-def handler(request: Request):
+# def handler(request: Request):
+#     return {
+#         "statusCode": 200,
+#         "body": "Hello, World!"
+#     }
+
+def handler(event, context):
     return {
         "statusCode": 200,
         "body": "Hello, World!"
